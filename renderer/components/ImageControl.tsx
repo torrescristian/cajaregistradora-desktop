0f8172e2730f19f38f0a1b4169f4d406a3dd77a5
Cristian T. Torres <cristiantomastorres@gmail.com>
2023-10-21 03:04:58 -0300

    version 0.2.2: fix upload images url

--- renderer/components/ImageControl.tsx
+++ renderer/components/ImageControl.tsx
@@ -3,6 +3,7 @@ import { IProduct } from '@/interfaces/IProduct';
 import strapi from '@/libs/strapi';
 import React from 'react';
 import Loader from './Loader';
+import { getStrapiUrl } from '../../main/helpers/utils';
 
 interface IProps {
   product?: IProduct;
@@ -13,20 +14,16 @@ const ImageControl = ({ product }: IProps) => {
 
   const handleSubmitForm = async (e: any) => {
     e.preventDefault();
-    const [productImage] = await fetch(
-      'https://control.cajaregistradora.app/api/upload',
-      {
-        method: 'POST',
-        body: new FormData(e.target),
-        headers: {
-          Authorization: 'Bearer ' + strapi.getToken(),
-          'Access-Control-Allow-Origin':
-            'https://control.cajaregistradora.app/',
-          'Access-Control-Allow-Methods': 'POST',
-          'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-        },
+    const [productImage] = await fetch(getStrapiUrl(), {
+      method: 'POST',
+      body: new FormData(e.target),
+      headers: {
+        Authorization: 'Bearer ' + strapi.getToken(),
+        'Access-Control-Allow-Origin': getStrapiUrl(),
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type',
       },
-    ).then((res) => res.json());
+    }).then((res) => res.json());
     updateProductMutation.mutate({
       id: product?.id!,
       image: productImage,
