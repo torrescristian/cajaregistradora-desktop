d55a54bf73ac8e983dc123b596c8e75f52b640f9
Cristian T. Torres <cristiantomastorres@gmail.com>
2024-02-22 09:46:23 -0300

    fix: build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './renderer/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./renderer/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
