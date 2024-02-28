96fd04c54fca4a95f6490d44946a70a2207c0484
Rodrigo Cabrera <cabrerarodrigo481@gmail.com>
2024-02-28 09:29:09 -0300

    Fix eliminar registro de la tabla deposit

--- renderer/modules/depositar/organisms/ColumnsAddToBalance.tsx
+++ renderer/modules/depositar/organisms/ColumnsAddToBalance.tsx
@@ -3,7 +3,6 @@ import { format } from 'date-fns';
 import { formatPrice } from '@/modules/common/libs/utils';
 import { INewAddBalance } from '@/modules/caja/interfaces/INewAddBalance';
 import { TrashIcon } from '@heroicons/react/24/solid';
-import ButtonCancelAddToBalance from '../moleculas/ButtonCancelAddToBalance';
 
 const columnHelper = createColumnHelper<INewAddBalance>();
 
@@ -21,10 +20,4 @@ export const columnsDefExp = [
     accessorKey: 'reason',
     header: 'Motivo',
   },
-  columnHelper.display({
-    header: 'Eliminar',
-    cell: (info) => (
-      <ButtonCancelAddToBalance newAddBalance={info.row.original} />
-    ),
-  }),
 ];
