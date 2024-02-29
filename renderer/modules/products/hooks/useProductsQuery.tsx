d1ba354d95a34fc54cdbfda5e4b8eacebcc57dc8
Rodrigo Cabrera <cabrerarodrigo481@gmail.com>
2024-02-29 11:35:10 -0300

    Fix bug se muentran elementos eliminados en pedidos

--- renderer/modules/products/hooks/useProductsQuery.tsx
+++ renderer/modules/products/hooks/useProductsQuery.tsx
@@ -104,6 +104,7 @@ export default function useProductsQuery({
 
         if (selectedProductType) {
           options.filters = {
+            ...options.filters,
             type: selectedProductType,
           };
         }
