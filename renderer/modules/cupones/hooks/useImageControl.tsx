1768c0338c60a6e85fad81325407b40bba2fc29c
Rodrigo Cabrera <cabrerarodrigo481@gmail.com>
2024-02-29 10:14:16 -0300

    Fix error al no subir imagen

--- renderer/modules/cupones/hooks/useImageControl.tsx
+++ renderer/modules/cupones/hooks/useImageControl.tsx
@@ -9,7 +9,10 @@ export const useImageControl = () => {
     const imageFile = formData.get('files');
 
     // Verificar si se proporcionó un archivo de imagen
-    if (imageFile instanceof File || (typeof imageFile === 'string' && imageFile.length > 0)) {
+    if (
+      imageFile instanceof File ||
+      (typeof imageFile === 'string' && imageFile.length > 0)
+    ) {
       try {
         const response = await fetch(`${getStrapiUrl()}/api/upload`, {
           method: 'POST',
@@ -38,20 +41,25 @@ export const useImageControl = () => {
       formData.delete('files');
 
       try {
-        const response = await fetch(`${getStrapiUrl()}/api/submit-form-without-image`, {
-          method: 'POST',
-          body: formData,
-          headers: {
-            Authorization: 'Bearer ' + strapi.getToken(),
-            'Access-Control-Allow-Origin': getStrapiUrl(),
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+        const response = await fetch(
+          `${getStrapiUrl()}/api/submit-form-without-image`,
+          {
+            method: 'POST',
+            body: formData,
+            headers: {
+              Authorization: 'Bearer ' + strapi.getToken(),
+              'Access-Control-Allow-Origin': getStrapiUrl(),
+              'Access-Control-Allow-Methods': 'POST',
+              'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+            },
           },
-        });
+        );
 
         if (!response.ok) {
           const errorData = await response.json();
-          throw new Error(errorData.message || 'Error al enviar el formulario sin imagen');
+          throw new Error(
+            errorData.message || 'Error al enviar el formulario sin imagen',
+          );
         }
 
         return null;
