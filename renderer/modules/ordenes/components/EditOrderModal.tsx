0edcca377690c3d39b97ce8da1ec5e8ac76438e0
Rodrigo Cabrera <cabrerarodrigo481@gmail.com>
2024-03-07 12:53:58 -0300

    bug añade producto al editar, no actualiza

--- renderer/modules/ordenes/components/EditOrderModal.tsx
+++ renderer/modules/ordenes/components/EditOrderModal.tsx
@@ -8,12 +8,13 @@ import {
 } from '@/modules/common/contexts/useOrderStore';
 
 export const EditOrderModal = () => {
+  //Añade el producto pero no lo actualiza
   const updateTakeAway = useOrderStore(getUpdateTakeAway);
   const { handleToggleEdit } = useContext(OrderContext);
   return (
     <button
       className="btn btn-outline text-base-content"
-      onClick={handleToggleEdit}
+      onClick={updateTakeAway}
     >
       <PencilIcon className="w-5 h-5 " />
     </button>
